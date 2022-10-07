621ff813ea4fd9a5b7d138e25e3df89dce2b6db2
msKim92 <mins402kim@gmail.com>
2022-10-07 11:07:35 +0900

    first commit

---
+++ src/test/java/com/codesatates/practicegithubActiondeploy/PracticeGithubActionDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesatates.practicegithubActiondeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeGithubActionDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
